c746defdc8710f56b890d4a911288ab0edeb08e8
tuanchan <anhtuan1811006@gmail.com>
2025-09-15 16:13:42 +0700

    Dictionary fix

--- Model/DashboardStudent.cs
+++ Model/DashboardStudent.cs
@@ -13,8 +13,8 @@ namespace StudentDashboardApp.Model
 {
     public partial class DashboardStudent : DevExpress.XtraBars.Ribbon.RibbonForm
     {
-        private NavigationHelper navHelper;// khai báo fild để tạo cho đối tượng trong DashboardStudent_Load
 
+        private NavigationHelper navHelper;
         public DashboardStudent()
         {
             InitializeComponent();
@@ -52,14 +52,23 @@ namespace StudentDashboardApp.Model
         {
 
         }
-     
 
+        
         private void DashboardStudent_Load(object sender, EventArgs e)
         {
-            navHelper = new NavigationHelper(navigationFrameSTD); // tạo đối tượng để sử dụng hàm trong class đc tạo
-            navigationFrameSTD.AllowTransitionAnimation = DevExpress.Utils.DefaultBoolean.False; // bỏ hiệu ứng chuyển nav
-            navigationFrameSTD.SelectedPage = navigationPageEmpty; // chọn navpage rỗng làm mặc định 
-       
+            var pageMap = new Dictionary<BarButtonItem, NavigationPage>()
+    {
+                // thêm nút khác và page cần hiển thị tại đây
+        { barButtonItemFindStudent, navigationPageFindStudent },
+        { barButtonItemAddST, navigationPageAddST },
+      
+        
+    };
+
+            navHelper = new NavigationHelper(navigationFrameSTD, pageMap); // đưa cho hàm ăn tham số của frame và Dictionary lần lượt là navigationFrameSTD và pageMap
+            navigationFrameSTD.AllowTransitionAnimation = DevExpress.Utils.DefaultBoolean.False; // tắt transition của frame    
+            navHelper.ShowEmptyPage(navigationPageEmpty);// mặc định khi mở form sễ tự chọn form rỗng thay vì trả frame về false sẽ dễ gay ra lỗi linh tinh gì gì đó
+
         }
 
 
@@ -100,7 +109,7 @@ namespace StudentDashboardApp.Model
 
         private void ribbon_SelectedPageChanged(object sender, EventArgs e)
         {
-            navigationFrameSTD.SelectedPage = navigationPageEmpty; // mỗi khi chuyển ribbon sẽ tự chọn lại navpage rỗng 
+            navHelper.ShowEmptyPage(navigationPageEmpty);// mỗi khi chuyển page sẽ tự động trả về page rỗng
         }
 
         private void ribbon_Click(object sender, EventArgs e)
@@ -111,13 +120,13 @@ namespace StudentDashboardApp.Model
         private void barButtonItemFindStudent_ItemClick(object sender, ItemClickEventArgs e)
         {
 
-            navHelper.ShowNavigationPage(navigationPageFindStudent); // gáng những page muốn mở vào những sự kiện của nút như đây là ví dụ điển hình, làm tương tự như những cái ở dưới
+           
         }
 
         private void barButtonItemAddST_ItemClick(object sender, ItemClickEventArgs e)
         {
 
-            navHelper.ShowNavigationPage(navigationPageAddST);
+           
         }
     }
 }
--- Services/NavigationHelper.cs
+++ Services/NavigationHelper.cs
@@ -1,24 +1,44 @@
 ﻿using System.Collections.Generic;
+using DevExpress.XtraBars;
 using DevExpress.XtraBars.Navigation;
-using DevExpress.XtraBars.Ribbon;
-using StudentDashboardApp.Model;
 
 namespace StudentDashboardApp.Services
 {
     public class NavigationHelper
     {
-        //fild
         private readonly NavigationFrame _frame;
-        //constructor
-        public NavigationHelper(NavigationFrame frame)
+        private readonly Dictionary<BarButtonItem, NavigationPage> _pageMap;
+
+        public NavigationHelper(NavigationFrame frame, Dictionary<BarButtonItem, NavigationPage> pageMap)// khởi tạo hàm có tham số truyền vào để nhận diện các frame và page
         {
-            _frame = frame;
+            _frame = frame; // đưa giá trị frame được truyền vào
+            _pageMap = pageMap;// đư agias trị các page và các nút được truyền vào
+
+            
+            foreach (var btn in _pageMap.Keys) //duyệt qua từng phần tử nút trong page map vừa lưu và khi có dấu hiệu ấn thì hàm OnButtonClick sẽ được thực thi (_pageMap.Keys lấy tất cả các key có trong hashmap)
+                btn.ItemClick+= OnButtonClick; // gắn thuộc tính sự kiện cho từng nút trong Dictionary vì thế kh cần sử dụng hàm sự kiện của form nữa chỉ cần khai bào trong load của form thôi nhó hiếu-chan
         }
-        //funtion
-        public void ShowNavigationPage(NavigationPage Page)
+
+        private void OnButtonClick(object sender, ItemClickEventArgs e)
         {
+            if (e.Item is BarButtonItem btn && _pageMap.TryGetValue(btn, out var page))// nếu bạn ấn vào nút dạng baritem đưa name của nút đó vào btn và trygetvalue kiểm tra xem xong hashmap Dictionary có tồn tại page tương ứng với name của button không, nếu có thì show page
+            {
+                ShowNavigationPage(page);
+            }
+        }
+
+        public void ShowNavigationPage(NavigationPage page)
+        {
+            if (page == null) return; // nếu biến page chưa được gán giá trị của page nào tắt hàm
+            _frame.Visible = true; // qua được phần điều kiện thì hiển thị lại frame
+            _frame.SelectedPage = page;// và chọn page đc tham chiếu trong Dictionary
+        }
+
+        public void ShowEmptyPage(NavigationPage emptyPage) // cái này kh cần sài cx được nhưng muốn ngầu hơn thì sài cái này
+        {
+            if (emptyPage == null) return;
             _frame.Visible = true;
-            _frame.SelectedPage = Page;
+            _frame.SelectedPage = emptyPage;
         }
     }
 }
